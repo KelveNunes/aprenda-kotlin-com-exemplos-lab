84b426c626434b40a9147b2471702558b7743f87
kelve <kelvenunes123@gmail.com>
2024-02-16 17:29:53 -0300

    feat: class annotations

---
+++ src/main/kotlin/Annotation/conteudos.kt
@@ -0,0 +1,8 @@
+package Annotation
+
+annotation class conteudos
+annotation class usarios
+annotation class formacao
+@Target(AnnotationTarget.EXPRESSION)
+@Retention(AnnotationRetention.SOURCE)
+annotation class MATRICULA
